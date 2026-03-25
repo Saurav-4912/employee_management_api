f73a68eb7143c95635f929c14fabfdfdb0cbc1f0
Saurav Pawar <sauravpawar46@gmail.com>
2026-03-25 14:53:00 +0530

    Final project

--- src/main/java/com/employee/exception/EmployeeAlreadyExistsException.java
+++ src/main/java/com/employee/exception/EmployeeAlreadyExistsException.java
@@ -1,7 +1,10 @@
 package com.employee.exception;
 
+
+
 public class EmployeeAlreadyExistsException extends RuntimeException {
     public EmployeeAlreadyExistsException(String message) {
         super(message);
     }
 }
+
